280193f99075d23f0a283c76d524cee8d2b6ba8f
Federico <gotfede@gmail.com>
2022-05-19 12:09:48 +0100

    many

--- variable_intro/strings_into.py
+++ variable_intro/strings_into.py
@@ -1,4 +1,4 @@
-h = "hello world!"
+
 
 double = "double quote"
 single = 'single quote'
@@ -9,6 +9,11 @@ failure = 'I said "oh no!"'
 both = 'I said "oh no david\'s asdasdas"'
 print(both)
 
-
+h = "hello world!"
 print(h[6]) #Print the caracter we want
 print(h[-1]) #Print the caracter we want backwards
+print(h[1:6]) #print part of the string
+
+
+#from slicing h, print "lo wo"
+print(h[3:8])
