9b51af11c85d0b368ac53a4a36eab296b17def01
Federico <gotfede@gmail.com>
2022-05-19 11:57:16 +0100

    start with python

---
+++ variable_intro/intro.py
@@ -0,0 +1,41 @@
+a = 1
+b = 2
+c = 3.5
+d = "Hello!"
+
+# print(a)
+# print(b)
+# print(c)
+# print(d)
+
+#this is a comment
+#python is a dynamically typed language
+# print(type(a))
+# print(type(b))
+# print(type(c))
+# print(type(d))
+#
+# print(a+b)
+# total = b + c
+# print(total)
+
+# what happends when you combine ints and floats with the different arithmetic operators?
+
+print(type(a))
+print(type(c))
+print(a+c)
+print(a*c)
+print(a-c)
+print(a/c)
+
+
+#complex number
+
+complex = 3 + 4j
+print(complex)
+
+a = 20
+b = 9
+
+
+print(a%b)
