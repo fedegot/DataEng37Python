9422759962d26faf4a111d9c1814943aaef63466
Federico <gotfede@gmail.com>
2022-05-19 12:06:18 +0100

    prepausa

---
+++ variable_intro/strings_into.py
@@ -0,0 +1,14 @@
+h = "hello world!"
+
+double = "double quote"
+single = 'single quote'
+
+print(double, single)
+
+failure = 'I said "oh no!"'
+both = 'I said "oh no david\'s asdasdas"'
+print(both)
+
+
+print(h[6]) #Print the caracter we want
+print(h[-1]) #Print the caracter we want backwards
