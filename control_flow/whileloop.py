5cde893e6517710a6ae5c0e417d8fd10a8aa32e6
Federico <gotfede@gmail.com>
2022-05-23 14:59:24 +0100

    Monday

--- control_flow/whileloop.py
+++ control_flow/whileloop.py
@@ -9,19 +9,32 @@
 #### lets do some fizz and buzz
 
 
-count = 0
-while count < 101:
-    if (count % 5) == 0 and (count % 3) == 0:
-        print("FizzBuzz")
-        count = count +1
-    elif(count % 3) == 0:
-        print("Fizz")
-        count = count + 1
-    elif (count % 5) == 0:
-        print("Buzz")
-        count = count +1
+# count = 0
+#
+# while count < 101:
+#     if (count % 5) == 0 and (count % 3) == 0:
+#         print("FizzBuzz")
+#         count = count +1
+#     elif(count % 3) == 0:
+#         print("Fizz")
+#         count = count + 1
+#     elif (count % 5) == 0:
+#         print("Buzz")
+#         count = count +1
+#     else:
+#         print(count)
+#         count = count + 1
+
+#@@@@@@@@@@@@@@@ AGE CONFIRMATION WITH WHILE LOOP @@@@@@@@@@@@@@@
+keep_asking = True
+
+while keep_asking:
+    age = input("What is your age?\n")
+    if age.isdigit():
+        age_int = int(age)
+        keep_asking = False
     else:
-        print(count)
-        count = count + 1
+        print("Please enter a valid number in digits.")
 
-
+print(f"Your Age is {age_int}")
+#@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
