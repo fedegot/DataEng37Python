f14f263b39d75967ea9f3a782d50301b4a544d02
Federico <gotfede@gmail.com>
2022-05-26 14:31:24 +0100

    TDD

--- control_flow/Unit_testing_TDD/simple_calc.py
+++ control_flow/Unit_testing_TDD/simple_calc.py
@@ -1,3 +1,12 @@
 class SimpleCalc:
     def add(self, num1, num2):
         return num1 + num2
+
+    def subtract(self, num1, num2):
+        return num1 - num2
+
+    def mult(self, num1, num2):
+        return num1 * num2
+
+    def div(self, num1, num2):
+        return num1 / num2
--- control_flow/Unit_testing_TDD/test_simple_calc.py
+++ control_flow/Unit_testing_TDD/test_simple_calc.py
@@ -8,3 +8,18 @@ class CalcTest(TestCase):
         actual = self.calc.add(2, 4)
         expected = 6
         self.assertEqual(actual, expected)
+
+    def test_subtract(self):
+        actual = self.calc.subtract(6, 3)
+        expected = 3
+        self.assertEqual(actual, expected)
+
+    def test_multiply(self):
+        actual  = self.calc.mult(2, 3)
+        expected = 6
+        self.assertEqual(actual, expected)
+
+    def test_division(self):
+        actual = self.calc.div(6, 2)
+        expected = 3
+        self.assertEqual(actual, expected)
