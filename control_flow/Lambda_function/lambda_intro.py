b28a8c08d1225603e756bd8a6a65409949909300
Federico <gotfede@gmail.com>
2022-05-26 11:45:45 +0100

    Scope

--- control_flow/Lambda_function/lambda_intro.py
+++ control_flow/Lambda_function/lambda_intro.py
@@ -1,11 +1,11 @@
 # LAMBDA OR ANONYMOUS FUNCTION
 
-
 # addition = lambda num1, num2: num1 + num2 + 1
 # print(addition(5, 7))
 
 # @#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#
-#map
+
+# map
 # saving = [242.00, 22.00, 50.00, 31.32]
 # # bonus = saving with 10% added on
 #
@@ -13,7 +13,9 @@
 # for x in saving:
 #     bonus.append(x+(x*0.1))
 # print(bonus)
+
 # @#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#
+
 # saving = [242.00, 22.00, 50.00, 31.32]
 # def apply_bonus(saving):
 #     return saving * 1.1
@@ -24,8 +26,47 @@
 # @#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#
 
 # MAP with LAMBDA
-saving = [242.00, 22.00, 50.00, 31.32]
-bonus_lambda = list(map(lambda x: x * 1.1, saving))
-print(bonus_lambda)
+# saving = [242.00, 22.00, 50.00, 31.32]
+# bonus_lambda = list(map(lambda x: x * 1.1, saving))
+# print(bonus_lambda)
+# print(list(f"{sb:.2f}" for sb in saving))
 
 # @#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#
+
+# Use map and lambda to create a list of each number squared plus 3
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+ans = map(lambda x: (x ** 2) + 3, numbers)
+print(list(ans))
+
+evens = filter(lambda x: x % 2 == 0, ans)
+print(list(evens)) # FILTER( wants to return a function which return a boolean
+
+# @#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#@#
+
+jobs = ["Data Analyst", "C#  Developer", "Data Engineer", "DevOps Engineer", "Data Architect"]
+# using map and filter with lambdas
+# product a list of the database roles without the word data in there
+# i.e. ["Analyst", "Engineer", "Architect"]
+
+eng = filter(lambda x: "Engineer" in x, jobs)
+analyst = filter(lambda x: "Analyst" in x, jobs)
+dev = filter(lambda x: "Developer" in x, jobs)
+
+data_job = map(lambda x: x[4:], jobs)
+
+#eng = filter(lambda y: y == True, ans2)
+print(list(data_job))
+print(list(eng))
+print(list(analyst))
+print(list(dev))
+
+
+
+
+
+
+
+
+
+
---
+++ control_flow/Lambda_function/scope.py
@@ -0,0 +1,51 @@
+# LEGB
+
+# LOCAL - ( Local scope refers to the name(variable, function and so on) that is defined inside the function.
+#           It means it is accessible within the point at it is defined until the end of the function.
+#           That particular scope can’t be modified or even accessed outside the function. As an example,
+#
+#           def example():
+#           x="LEGB rule"
+#           print(x)
+#
+#           example()
+#
+#           print(x)
+#           x variable can be accessible only inside the example() function so, when we attempt to use it as print(x)
+#           it gives no error. However when we try to reach it from outside of the function(below we call example function),
+#           there an error arises:
+#
+#           NameError: name 'x' is not defined LEGB rule
+#
+#           It is because, local scope isn’t visible outside the function where it is created. )
+
+# ENCLOSED - (Enclosing scope is kind of scope that exists only in nested functions
+#            (A function defined inside another function is called a nested function).
+#            The enclosing scope is visible only for inner and enclosing functions. )
+
+# GLOBAL -  (Global scope contains all the names(variables, functions, objects and so on) that you define at the highest
+#           level in your program. It means that we can use that names from anywhere we wish.)
+
+
+# str = "global function"
+#
+#
+# def example():
+#     def example_inner():
+#         print(str)
+#
+#     print(str)
+#
+#     example_inner()
+#
+#
+# example()
+# Output
+# will
+# be as,
+#
+# global function
+# global function
+
+
+# BUILT-IN - (
