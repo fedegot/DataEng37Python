637ebcd6d26f0ef3fbf4001ab8e888ffc2446a50
Federico <gotfede@gmail.com>
2022-05-27 11:08:36 +0100

    Exchange

---
+++ control_flow/JSON_Japes/money_zone.py
@@ -0,0 +1,9 @@
+from rates_parser import RatesParser
+
+rp = RatesParser("exchange_rates.json")
+
+print(f"{rp.date}: {rp.base_rate} to GBP - {rp.gbp}")
+
+
+wallet = 142
+print(rp.to_GBP(wallet))
---
+++ control_flow/JSON_Japes/rates_parser.py
@@ -0,0 +1,26 @@
+import json
+
+
+class RatesParser:
+    # retrieve the data from json file
+    def __init__(self, filepath):
+        rates_data = self._open_json_file(filepath)
+        self.base_rate = rates_data["base"]
+        self.date = rates_data["date"]
+        self.gbp = rates_data["rates"]["GBP"]
+        self.usd = rates_data["rates"]["USD"]
+        self.jpy = rates_data["rates"]["JPY"]
+
+    # open file json file
+    def _open_json_file(self, filepath: str) -> dict:
+        with open(filepath) as jsonfile:
+            return json.load(jsonfile)
+
+    def to_GBP(self, amount):
+        return amount * self.gbp
+
+if __name__ == "__main__":
+    rp = RatesParser("exchange_rates.json")
+    print(rp.gbp)
+    # rates = rp._open_json_file("exchange_rates.json")
+    # print(rates)
